9292465a2fa8cf480242418f37f01f3bca60bf89
Fassam Khan <muhammadfassam465@gmail.com>
2026-02-19 15:55:44 +0500

    Update Maintainance component with image and maintenance message

--- Components/Maintainance.jsx
+++ Components/Maintainance.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Maintainance = () => {
   return (
-    <div className='w-[100vw] h-[100vh]'>
-        We are working on it
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center flex-col gap-4 p-3' >
+        <Image src={'/Images/maintainance.webp'} height={180} width={300} alt='maintainance'/>
+        <div className='md:w-[600px] w-[100%] text-center'>
+        <p>Our website is under maintenance at the moment.
+To place your order online, kindly contact us via WhatsApp at 0336-2234470.
+Thank you for your support!</p>
+
+        </div>
+       
+       
       
     </div>
   )
