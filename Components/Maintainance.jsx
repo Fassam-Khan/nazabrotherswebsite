95ce73232791995407d7461839b0fcb6f12adfdb
Fassam Khan <muhammadfassam465@gmail.com>
2026-02-19 15:42:59 +0500

    Add Maintainance component to RootLayout

---
+++ Components/Maintainance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Maintainance = () => {
+  return (
+    <div className='w-[100vw] h-[100vh]'>
+        We are working on it
+      
+    </div>
+  )
+}
+
+export default Maintainance
