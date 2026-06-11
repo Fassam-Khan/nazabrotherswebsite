551d4914d9bdd335233ed4218c89647ff6ab4d66
Fassam Khan <muhammadfassam465@gmail.com>
2026-06-12 00:36:44 +0500

    changes in ui

--- Components/Categories.jsx
+++ Components/Categories.jsx
@@ -13,14 +13,14 @@ const Categories = async () => {
 
 
     return (
-        <div className='mt-10 '>
+        <div className='mt-10 md:p-0 p-2 '>
             {/* Heading  */}
             <div className='flex justify-between items-center border-b-2 border-[#c2c2c241] h-[60px]'>
                 <div> <h2 className='text-2xl font-extrabold'>Popular categories</h2></div>
                 <div className='hidden md:flex'><a href="#">View All Categories</a></div>
             </div>
             {/* Categories  */}
-            <div className='grid grid-cols-5 w-[100%] grid-cols-2 gap-8 mt-6 justify-items-center'>
+            <div className='grid md:grid-cols-5 w-[100%]  grid-cols-2 gap-8 mt-6 justify-items-center'>
                 {filterCategories.map((e, index) => (
                     <div
                         key={index}
