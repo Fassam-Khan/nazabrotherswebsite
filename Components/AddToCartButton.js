50d41e69cee9b40fdbde09a01dc70d4cc15b9e46
Fassam Khan <muhammadfassam465@gmail.com>
2026-06-13 00:38:51 +0500

    fix error

--- Components/AddToCartButton.js
+++ Components/AddToCartButton.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { ShoppingCart, Check } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import { useCart } from "@/app/context/CartContext";
 
 export default function AddToCartButton({ product, className, variant = "default", size = "default" }) {
